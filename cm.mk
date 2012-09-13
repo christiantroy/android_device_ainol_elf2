47c9d47f8fada1e25dad19ef884d6eb03872e558
Alan Marchesan <justcavalli@gmail.com>
2012-09-13 08:14:46 +0200

    Spoof device as Nexus 7

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Setup device specific product configuration.
 #
 PRODUCT_NAME := cm_elf2
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := Google
 PRODUCT_DEVICE := elf2
-PRODUCT_MODEL := GT-I9100
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.0.3/IML74K/XXLPQ:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.0.3 IML74K XXLPQ release-keys"
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi TARGET_DEVICE=grouper BUILD_FINGERPRINT="google/nakasi/grouper:4.1.1/JRO03D/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03D 402395 release-keys"
