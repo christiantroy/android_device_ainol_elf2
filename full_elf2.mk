732a344910a8ff90d52d8f5c54fa1edebbc9bb1e
Alan Marchesan <justcavalli@gmail.com>
2012-10-01 13:54:55 +0200

    Typo

--- full_elf2.mk
+++ full_elf2.mk
@@ -26,7 +26,7 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilt boot animation & PPP Widget
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/bootanimation.zip:system/media/bootanimation.zip
+    $(LOCAL_PATH)/prebuilt/bootanimation.zip:system/media/bootanimation.zip \
     $(LOCAL_PATH)/prebuilt/PPPWidget.apk:system/app/PPPWidget.apk
 
 # init.d scripts
