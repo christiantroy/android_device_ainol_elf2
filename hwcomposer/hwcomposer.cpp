4387613d83ff44f43cba6ecd45646cbf960dea16
Alan Marchesan <justcavalli@gmail.com>
2012-11-24 09:54:06 +0100

    jb 4.2 changes

--- hwcomposer/hwcomposer.cpp
+++ hwcomposer/hwcomposer.cpp
@@ -15,6 +15,8 @@
  */
 
 //#define LOG_NDEBUG 0
+#define HWC_REMOVE_DEPRECATED_VERSIONS 1
+
 #define LOG_TAG "hwcomposer"
 #include <hardware/hardware.h>
 
@@ -37,9 +39,9 @@
 /*****************************************************************************/
 
 struct hwc_context_t {
-    hwc_composer_device_t device;
+    hwc_composer_device_1_t device;
     /* our private state goes below here */
-    hwc_layer_t const* saved_layer;
+    hwc_layer_1_t const* saved_layer;
     unsigned saved_transform;
     int saved_left;
     int saved_top;
@@ -70,7 +72,7 @@ hwc_module_t HAL_MODULE_INFO_SYM = {
 
 /*****************************************************************************/
 
-static void hwc_overlay_compose(hwc_composer_device_t *dev, hwc_layer_t const* l) {
+static void hwc_overlay_compose(hwc_composer_device_1_t *dev, hwc_layer_1_t const* l) {
     int angle;
     struct hwc_context_t* ctx = (struct hwc_context_t*)dev;
 
@@ -114,7 +116,7 @@ static void hwc_overlay_compose(hwc_composer_device_t *dev, hwc_layer_t const* l
     ctx->saved_bottom = l->displayFrame.bottom;
 }
 
-static void dump_layer(hwc_layer_t const* l) {
+static void dump_layer(hwc_layer_1_t const* l) {
     ALOGD("\ttype=%d, flags=%08x, handle=%p, tr=%02x, blend=%04x, {%d,%d,%d,%d}, {%d,%d,%d,%d}",
             l->compositionType, l->flags, l->handle, l->transform, l->blending,
             l->sourceCrop.left,
@@ -127,51 +129,42 @@ static void dump_layer(hwc_layer_t const* l) {
             l->displayFrame.bottom);
 }
 
-static int hwc_prepare(hwc_composer_device_t *dev, hwc_layer_list_t* list) {
+static int hwc_prepare(hwc_composer_device_1_t *dev, size_t numDisplays, hwc_display_contents_1_t** displays) {
+    // Compat
+    hwc_display_contents_1_t* list = NULL;
+    if (numDisplays > 0) {
+	list = displays[0];
+    }
+
     if (list && (list->flags & HWC_GEOMETRY_CHANGED)) {
         for (size_t i=0 ; i<list->numHwLayers ; i++) {
-            hwc_layer_t* l = &list->hwLayers[i];
-#if 0
-            //dump_layer(l);
-            if (l->handle) {
-                private_handle_t const* hnd = reinterpret_cast<private_handle_t const*>(l->handle);
-                if (hnd->flags & private_handle_t::PRIV_FLAGS_VIDEO_OVERLAY) {
-                    l->hints = HWC_HINT_CLEAR_FB;
-                    l->compositionType = HWC_OVERLAY;
-                    continue;
-                }
-            }
-#endif
+            hwc_layer_1_t* l = &list->hwLayers[i];
             l->compositionType = HWC_FRAMEBUFFER;
         }
     }
     return 0;
 }
 
-static int hwc_set(hwc_composer_device_t *dev,
-        hwc_display_t dpy,
-        hwc_surface_t sur,
-        hwc_layer_list_t* list)
+static int hwc_set(hwc_composer_device_1_t *dev, size_t numDisplays, hwc_display_contents_1_t** displays)
 {
+    
+    // Only support one display
+    hwc_display_t dpy = displays[0]->dpy;
+    hwc_surface_t sur = displays[0]->sur;
+    hwc_display_contents_1_t* list = displays[0];
+
     if (list == NULL) {
         return 0;
     }
 
     for (size_t i=0 ; i<list->numHwLayers ; i++) {
-        hwc_layer_t* l = &list->hwLayers[i];
-#if 0
-        if (l->compositionType == HWC_OVERLAY) {
-            //dump_layer(l);
-            hwc_overlay_compose(dev, l);
-        }
-#else
+        hwc_layer_1_t* l = &list->hwLayers[i];
         if (l->handle) {
             private_handle_t const* hnd = reinterpret_cast<private_handle_t const*>(l->handle);
             if (hnd->flags & private_handle_t::PRIV_FLAGS_VIDEO_OVERLAY) {
                 hwc_overlay_compose(dev, l);
             }
         }
-#endif
     }
 
     EGLBoolean sucess = eglSwapBuffers((EGLDisplay)dpy, (EGLSurface)sur);
@@ -179,6 +172,21 @@ static int hwc_set(hwc_composer_device_t *dev,
         return HWC_EGL_ERROR;
     }
     return 0;
+}
+
+static int hwc_blank(struct hwc_composer_device_1 *dev, int disp, int blank)
+{
+    return 0;
+}
+
+static int hwc_eventControl(struct hwc_composer_device_1* dev, int dpy,
+        int event, int enabled)
+{
+    switch (event) {
+      case HWC_EVENT_VSYNC:
+	  return 0;
+    }
+    return -EINVAL;
 }
 
 static int hwc_device_close(struct hw_device_t *dev)
@@ -205,12 +213,14 @@ static int hwc_device_open(const struct hw_module_t* module, const char* name,
 
         /* initialize the procs */
         dev->device.common.tag = HARDWARE_DEVICE_TAG;
-        dev->device.common.version = 0;
+        dev->device.common.version = HWC_DEVICE_API_VERSION_1_0;
         dev->device.common.module = const_cast<hw_module_t*>(module);
         dev->device.common.close = hwc_device_close;
 
         dev->device.prepare = hwc_prepare;
         dev->device.set = hwc_set;
+	dev->device.blank = hwc_blank;
+	dev->device.eventControl = hwc_eventControl;
 
         *device = &dev->device.common;
         status = 0;
