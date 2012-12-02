5d09392558ba13b6549b880414d983c3c03429bc
Alan Marchesan <justcavalli@gmail.com>
2012-12-02 09:17:46 +0100

    Remove fixpermissions

--- full_elf2.mk
+++ full_elf2.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.d/02modules:system/etc/init.d/02modules \
     $(LOCAL_PATH)/init.d/03kernel:system/etc/init.d/03kernel \
-    $(LOCAL_PATH)/init.d/06fixpermissions:system/etc/init.d/06fixpermissions \
     $(LOCAL_PATH)/init.d/10devlistener:system/etc/init.d/10devlistener \
     $(LOCAL_PATH)/init.d/11link2sd:system/etc/init.d/11link2sd
 
